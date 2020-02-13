53b4bef3aecca2b3a5101817378db7929caa174b
Felipe Cantu <cantujr.felipe@gmail.com>
2020-02-12 21:07:07 -0600

    files added and start of game
    start of the html with basic container,
    start of javascript with random character created, css is empty.

---
+++ assets/JavaScript/game.js
@@ -0,0 +1,17 @@
+
+var guessNumText = document.getElementById("guessleft");
+var lossText = document.getElementById("lost");
+var winText = document.getElementById("won");
+var guessText = document.getElementById("guesses");
+// generate random numbers
+
+var characters = 'abcdefghijklmnopqrstuvwxyz';
+var generator = characters.charAt(Math.floor(Math.random() * characters.length));
+
+console.log(generator);
+
+
+function gamePlay() {
+
+}
+
